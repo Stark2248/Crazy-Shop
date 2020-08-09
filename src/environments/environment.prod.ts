dae98ff59432894d7030532d08347c573ec95689
Stark2248 <49605278+Stark2248@users.noreply.github.com>
2020-08-09 22:25:06 +0530

    Add files via upload

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDd0cZ0UeJOaz6OzqmLPJSfI1SjyXjbV1k',
+    authDomain: 'crazy-shop-22944.firebaseapp.com',
+    databaseURL: 'https://crazy-shop-22944.firebaseio.com',
+    projectId: 'crazy-shop-22944',
+    storageBucket: 'crazy-shop-22944.appspot.com',
+    messagingSenderId: '519398487091',
+    appId: '1:519398487091:web:25e690b303b8d764f35f46',
+    measurementId: 'G-JVDMY7WRJW'
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDd0cZ0UeJOaz6OzqmLPJSfI1SjyXjbV1k',
+    authDomain: 'crazy-shop-22944.firebaseapp.com',
+    databaseURL: 'https://crazy-shop-22944.firebaseio.com',
+    projectId: 'crazy-shop-22944',
+    storageBucket: 'crazy-shop-22944.appspot.com',
+    messagingSenderId: '519398487091',
+    appId: '1:519398487091:web:25e690b303b8d764f35f46',
+    measurementId: 'G-JVDMY7WRJW'
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
