bcfe9e83424e8f8b686e1c203b9c878003952e5c
Amlan Das <dasamlan71@gmail.com>
2023-06-14 16:27:50 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,13 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: 'AIzaSyDd0cZ0UeJOaz6OzqmLPJSfI1SjyXjbV1k',
+    apiKey: <api key>,
     authDomain: 'crazy-shop-22944.firebaseapp.com',
     databaseURL: 'https://crazy-shop-22944.firebaseio.com',
     projectId: 'crazy-shop-22944',
     storageBucket: 'crazy-shop-22944.appspot.com',
     messagingSenderId: '519398487091',
-    appId: '1:519398487091:web:25e690b303b8d764f35f46',
+    appId: '<api id>',
     measurementId: 'G-JVDMY7WRJW'
   }
 };
